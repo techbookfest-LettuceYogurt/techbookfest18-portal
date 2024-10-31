bf1377a72359f0f366d7cfedd5d1d59c6e28a41b
kume-negitoro <kume.negitoro@gmail.com>
2024-11-01 03:07:25 +0900

    fix image src

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,16 +1,12 @@
 import { ThemeConfig } from 'nextra'
 import Image from 'next/image'
+import logo from './public/images/logo.png'
 
 export default {
     logo: (
         <span className="flex items-end">
             <span className="mr-2">
-                <Image
-                    width="48"
-                    height="48"
-                    src="/images/logo.png"
-                    alt="logo"
-                />
+                <Image width="48" height="48" src={logo} alt="logo" />
             </span>
             <p>Lettuce Yogurt | 令和の新卒エンジニア</p>
         </span>
