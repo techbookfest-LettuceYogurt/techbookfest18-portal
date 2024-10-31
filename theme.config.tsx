92deb1cfcf1b30c36e2681c05ffd20259ff60210
kume-negitoro <kume.negitoro@gmail.com>
2024-11-01 02:40:32 +0900

    fix image tag

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,9 +1,17 @@
 import { ThemeConfig } from 'nextra'
+import Image from 'next/image'
 
 export default {
     logo: (
         <span className="flex items-end">
-            <img className="mr-2" width="48" src="/images/logo.png" />
+            <span className="mr-2">
+                <Image
+                    width="48"
+                    height="48"
+                    src="/images/logo.png"
+                    alt="logo"
+                />
+            </span>
             <p>Lettuce Yogurt | 令和の新卒エンジニア</p>
         </span>
     ),
