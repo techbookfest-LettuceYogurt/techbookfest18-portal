22f4fabbb3aeb6a1bf81cb604c77955c35234394
kume-negitoro <kume.negitoro@gmail.com>
2024-10-31 22:34:38 +0900

    setup nextra

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+import nextra from 'nextra'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
+const withNextra = nextra({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.tsx',
+})
 
-export default nextConfig;
+export default withNextra({
+    /* config options here */
+    reactStrictMode: true,
+})
---
+++ theme.config.tsx
@@ -0,0 +1,9 @@
+import { ThemeConfig } from 'nextra'
+
+export default {
+    logo: <span>My Nextra Documentation</span>,
+    project: {
+        link: 'https://github.com/LettuceYogurt/techbookfest17-portal',
+    },
+    // ... other theme options
+} satisfies ThemeConfig
