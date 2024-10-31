0856f5b4a531c0e3d9443d634f3a087968f9ecab
kume-negitoro <kume.negitoro@gmail.com>
2024-11-01 01:42:00 +0900

    add files

---
+++ src/pages/_meta.ts
@@ -0,0 +1,19 @@
+import { Meta } from 'nextra'
+
+export default {
+    index: '令和の新卒エンジニア',
+    eratta: '正誤表',
+    articles: '章別サポート',
+    techbookfest: {
+        title: '技術書典 ↗',
+        type: 'page',
+        href: 'https://techbookfest.org/product/42JrffL4fu51vRf718xaej',
+        newWindow: true,
+    },
+    x: {
+        title: '公式𝕏 ↗',
+        type: 'page',
+        href: 'https://x.com/LettuceYogurt',
+        newWindow: true,
+    },
+} satisfies Meta
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,9 +1,14 @@
 import { ThemeConfig } from 'nextra'
 
 export default {
-    logo: <span>My Nextra Documentation</span>,
+    logo: (
+        <span className="flex items-end">
+            <img className="mr-2" width="48" src="/images/logo.png" />
+            <p>Lettuce Yogurt | 令和の新卒エンジニア</p>
+        </span>
+    ),
     project: {
-        link: 'https://github.com/LettuceYogurt/techbookfest17-portal',
+        link: 'https://github.com/techbookfest-LettuceYogurt/techbookfest17-portal',
     },
     // ... other theme options
 } satisfies ThemeConfig
