b129296b328a8886e4f9d768fc45610326a02705
kume-negitoro <kume.negitoro@gmail.com>
2024-11-01 02:15:55 +0900

    add deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import nextra from 'nextra'
 
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
+
 const withNextra = nextra({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.tsx',
@@ -8,4 +10,12 @@ const withNextra = nextra({
 export default withNextra({
     /* config options here */
     reactStrictMode: true,
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    assetPrefix: urlPrefix,
+    basePath: urlPrefix,
+    trailingSlash: true,
+    publicRuntimeConfig: { urlPrefix },
 })
